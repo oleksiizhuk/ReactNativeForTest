b5f326d614134150a30eda74063c30d63a5be8ca
OleksiiZhuk <oleksiizhuk.att@gmail.com>
2023-11-10 12:21:49 +0100

    start android build

--- src/components/teamplate/Camera/Camera.tsx
+++ src/components/teamplate/Camera/Camera.tsx
@@ -1,14 +1,14 @@
 import { Text, TouchableOpacity, View } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+// import { RNCamera } from 'react-native-camera';
 import React, { memo } from 'react';
 
 export const CameraTemplate = memo(({ setCamera, takePicture }: any) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <RNCamera
-        ref={(ref: any) => setCamera(ref)}
-        style={{ width: 300, height: 400 }}
-      />
+      {/*<RNCamera*/}
+      {/*  ref={(ref: any) => setCamera(ref)}*/}
+      {/*  style={{ width: 300, height: 400 }}*/}
+      {/*/>*/}
       <TouchableOpacity onPress={takePicture}>
         <Text style={{ fontSize: 20, marginTop: 20 }}>Take Picture</Text>
       </TouchableOpacity>
