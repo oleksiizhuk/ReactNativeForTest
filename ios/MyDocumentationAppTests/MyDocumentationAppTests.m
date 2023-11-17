c1680acacf93df5061c03c24c28d43a1e2bc490a
OleksiiZhuk <oleksiizhuk.att@gmail.com>
2023-11-17 10:06:16 +0100

    rename app

--- ios/MyDocumentationAppTests/MyDocumentationAppTests.m
+++ ios/MyDocumentationAppTests/MyDocumentationAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MyAppTests : XCTestCase
+@interface MyDocumentationAppTests : XCTestCase
 
 @end
 
-@implementation MyAppTests
+@implementation MyDocumentationAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
