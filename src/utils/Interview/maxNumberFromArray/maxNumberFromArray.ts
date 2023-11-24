f62a7e207d12256720c47c1e43b4c1bf597dfe61
OleksiiZhuk <oleksiizhuk.att@gmail.com>
2023-11-24 09:00:32 +0100

    add several task

--- src/utils/Interview/maxNumberFromArray/maxNumberFromArray.ts
+++ src/utils/Interview/maxNumberFromArray/maxNumberFromArray.ts
@@ -10,15 +10,6 @@ export function maxNumberFromArray(first: number, ...rest: number[]) {
 
 export function maxNumberFromArrayWithArguments(..._rest: number[]) {
   let maxValue = -Infinity
-  console.log('Infinity = ', Infinity)
-  console.log('arguments = ', arguments as IArguments)
-  console.log('arguments = ', typeof arguments)
-  const obj = {
-    0: 1,
-    2: 1,
-    3: 1,
-  }
-  console.log(obj['0'])
   for (let i = 0; i < arguments.length; i++) {
     if (arguments[i] > maxValue) {
       maxValue = arguments[i]
